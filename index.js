ae960e9586892c04bbf9791a82ff69b8021efe29
Ian <16856101+peakyDicers@users.noreply.github.com>
2022-05-19 22:52:50 -0400

    ensure number and symbol.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 import { customAlphabet } from "nanoid";
 
-const n = customAlphabet(
-  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&"
+const nanoid = customAlphabet(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?~-_"
 );
 
-for (let i = 0; i < 5; i++) console.log(n(20));
+const nums = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+const symbols = new Set(["!", "@", "$", "%", "^", "&", "*", "?", "~", "-", "_"])
+
+const hasNumber = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    if (nums.has(s[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const hasSymbol = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    if (symbols.has(s[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+for (let i = 0; i < 5; i++){
+  let pass;
+  do {
+    pass = nanoid()
+  } while (!hasNumber(pass) || !hasSymbol(pass))
+  console.log(pass);
+}
